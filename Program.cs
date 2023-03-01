ddbecec31c34ab61258b1542638285a3ce11d425
Ugurcan <ugurcan380@gmail.com>
2023-03-01 15:21:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+string isim, soyad,tc_no, x, miktar;
+int miktarr;
+int bakiye = 500;
+Console.WriteLine("sırasıyla: isminizi, soyadınızı ve tc no. giriniz");
+isim = Console.ReadLine();
+soyad = Console.ReadLine();
+tc_no = Console.ReadLine();
+
+Console.WriteLine("para yatırmak istiyorsanız 1 - para çekmek istiyorsanız 2 " +
+                  "yazınız ve en son miktarınız yazınız.");
+x = Console.ReadLine();
+miktar = Console.ReadLine();
+miktarr = Convert.ToInt32(miktar);
+switch (x)
+{
+    case "1":
+        bakiye += miktarr;
+        Console.WriteLine("son bakiyeniz " + bakiye);
+        Console.WriteLine("paranız başarıyla eklendi");
+        break;
+    case "2":
+        bakiye -= miktarr;
+        Console.WriteLine("son bakiyeniz " + bakiye);
+        Console.WriteLine("paranızı başarıyla çektiniz");
+        break;
+}
+
+Console.ReadLine();
